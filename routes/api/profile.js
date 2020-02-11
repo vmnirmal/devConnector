f5a7e27ff44c6dde58a6555a0a4e6ddab788f071
Nirmal kumar <nirmalkumar.vasanthimohan@kohls.com>
2020-02-11 12:41:56 +0530

    profile experiences commtting

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,4 +116,87 @@ router.get('/user/:user_id',async(req,res)=>{
     }
     
 });
+
+// @route  Delete api/profile/
+// @desc   Get all profiles
+// @access Public (if this service used any token validation then this is private access)
+
+router.delete('/',auth, async(req,res) => {
+
+    await Profile.findOneAndRemove({user:req.user.id});
+    await User.findOneAndRemove({_id:req.user.id});
+    res.json({msg:'user deleted successfully !'});
+
+});
+
+// @route  Put api/profile/
+// @desc   Get all profiles
+// @access private (if this service used any token validation then this is private access)
+
+router.put('/',[auth,[
+    check('title','Title is required').notEmpty(),
+    check('company','company is required').notEmpty(),
+    check('from','from date is required').notEmpty()
+]], async(req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(401).json({errors:errors.array()});
+    }
+
+    const {
+        title,
+        company,
+        from,
+        location,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        from,
+        to,
+        current,
+        description,
+        location
+    }
+
+    try {
+        const profile = await Profile.findOne({user:req.user.id});
+        console.log(profile); 
+        if(!profile){
+            return res.status(401).json({msg:'No profile found'});
+        }
+        profile.experience.unshift(newExp);
+        await profile.save()
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('server error !');
+    }
+
+});
+
+// @route  Put api/profile/
+// @desc   Get all profiles
+// @access Public (if this service used any token validation then this is private access)
+
+router.delete('/experience/:exp_id',auth,async(req,res)=>{
+try {
+
+    const profile = await Profile.findOne({user: req.user.id});
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex,1)
+    await profile.save();
+    //let profile = await Profile.findOneAndRemove({experience:req.params.exp_id});
+    res.json(profile);
+} catch (err) {
+    console.error(err.message)
+    return res.status(500).send('Server error');
+}
+    
+});
 module.exports =router;
