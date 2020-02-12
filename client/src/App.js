6355e0b85e705b6174af8be348c71c98d3d0c2b7
Nirmal kumar <nirmalkumar.vasanthimohan@kohls.com>
2020-02-12 20:01:25 +0530

    add react code

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
+
+const  App =() => 
+  <Router>
+    <Fragment>
+      <Navbar></Navbar>
+      <Route exact path="/" component={Landing}/>
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Switch>
+        </section>
+    </Fragment>
+  </Router>
+export default App;
